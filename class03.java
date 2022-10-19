d3afe3db66c7aee0e1156fc5a9f0c9151e45ac81
gale46 <113490808+gale46@users.noreply.github.com>
2022-10-19 11:36:12 +0800

    Add files via upload

---
+++ class03.java
@@ -0,0 +1,36 @@
+
+public class j10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Compute cmp = new Compute();
+		cmp.mul(3, 5);
+		cmp.show();
+		Math.mth = new Math();
+		//abstract是抽象的，所以abstract不能建立物件
+
+	}
+
+}
+
+abstract class Math {
+	protected int ans;
+	protected void show() {};
+
+	public abstract void add(int a, int b);
+
+	public abstract void sub(int a, int b);
+
+	public abstract void mul(int a, int b);
+
+	public abstract void div(int a, int b);
+}
+class Compute extends Math{
+	public void mul(int a, int b) {
+		ans = a * b;
+	}
+	public void show() {
+		System.out.println(ans);
+	}
+	
+}
