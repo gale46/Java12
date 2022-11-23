2ac048b9213308deedbef914e2c8a9248f66c243
gale46 <113490808+gale46@users.noreply.github.com>
2022-11-23 11:37:15 +0800

    Update class08.java

--- class08.java
+++ class08.java
@@ -1,5 +1,7 @@
 // app12_6.java,���ɮ׸m��pack6��Ƨ���
 package pack6.subpack1.subsubpack;  // �Napp12_6���O�ǤJpackage pack6��
+import pack6.subpack1.CCircle;   // 匯入其他檔案
+import pack6.subpack2.CRectangle;   // ���Jpack6.subpack2�̪�CRectangle���O
 
 class CStyle{
 	public int style;
@@ -15,11 +17,9 @@ public class app12_6
    public static void main(String args[])
    {
 	  CStyle cs = new CStyle(1);//設置style為1
-	  /*
       CCircle cir=new CCircle();
       CRectangle rect=new CRectangle();
       cir.show();
       rect.show();
-      */
    }
 }
