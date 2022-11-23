6890814d4d1b979dee9726d5d7438029f466204a
gale46 <113490808+gale46@users.noreply.github.com>
2022-11-23 10:37:14 +0800

    Update and rename class02.java to class08.java

---
+++ class08.java
@@ -0,0 +1,18 @@
+package pack6.subpack1.subsubpack;
+class CStyle{
+	public int style;
+	public CStyle(int n) {
+		style = n;
+		//將style設為n
+		System.out.println("style"+style);
+		//輸出style
+	}
+}
+public class Java12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CStyle cs = new CStyle(1);//設置style為1
+	}
+
+}
