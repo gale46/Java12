ef1a8ef84b52b974c6e6d670106905c255c98da5
gale46 <113490808+gale46@users.noreply.github.com>
2022-12-01 17:09:46 +0800

    Create class15.java

---
+++ class15.java
@@ -0,0 +1,9 @@
+public class class15{
+    public static void main(String[] args) {
+        
+        String s = String.valueOf(123.45);
+        System.out.println("字串長度 = "+ s.length());
+        
+    }
+    
+}
