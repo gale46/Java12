0f038d7dd799bffae45e2ef7ca39646c56cc1579
gale46 <113490808+gale46@users.noreply.github.com>
2022-12-01 16:42:19 +0800

    Update class13.java

--- class13.java
+++ class13.java
@@ -1,6 +1,6 @@
 public class class13{
     public static void main(String[] args) {
-        System.out.println(Math.round(5.38));
+        System.out.println("Math.round(5.38) = "+Math.round(5.38));//round為四捨五入
         
     }
     
